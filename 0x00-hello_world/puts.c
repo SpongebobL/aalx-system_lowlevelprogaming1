53e6e8029eedf34993aeb82432df71c56b2f2bae
Spongebob longpants <spongeboblongpants9@gmail.com>
2022-09-12 19:37:18 -0700

    C

---
+++ 0x00-hello_world/puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * puts -my str- outputs mystr
+ */
+int main (void)
+{
+	char Line1[] = "#!/bin/bash";
+	char Mystr[] = "Programming is like building a multilingual puzzle";
+	puts(Line1);
+	puts(Mystr);
+	return 0;
+}
