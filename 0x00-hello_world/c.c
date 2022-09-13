9e0098291c72204371bd023e63de963a5fa4e8d9
Spongebob longpants <spongeboblongpants9@gmail.com>
2022-09-12 18:50:24 -0700

    C

--- 0x00-hello_world/c.c
+++ 0x00-hello_world/c.c
@@ -2,12 +2,14 @@
 
 /**
  * main - entry point
+ * print_#!/bi/bash -prints "#bin/bash"
  * print_I love you - prints "I love you"
  * return: value 0
  */
 int main(void)
 {
+	printf("#!/bin/bash\n");
 	printf("I love you\n");
-	return (0);
+	return(0);
 }
 
